fc01041da93f939af2ef5d1394f35102c93591aa
zn <iszhounie@aliyun.com>
2020-10-13 18:02:13 +0800

    init

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function About() {
+    return (
+        <div>
+            <Head>
+                <title>关于</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            about
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.less'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>首页</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
