0f06a90d7153f9758ca25423b50d9db54aae3fda
zhounie <iszhounie@aliyun.com>
2020-10-15 23:55:23 +0800

    添加footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,50 @@
 import styles from '../styles/footer.less'
+import icon from '../styles/iconfont.less'
+import { useEffect, useState } from 'react'
+import Router from 'next/router'
+import Link from 'next/link'
 
 export default function Footer() {
+    useEffect(() => {
+        
+    }, [])
 
+    const handleOpen = (url) => {
+        window.open(url)
+    }
     return (
         <footer>
-
+            <div className={styles.box}>
+                <ul className={styles.more}>
+                    <li>
+                        移动版
+                    </li>
+                    <li>
+                        <Link href="/about">关于我</Link>
+                    </li>
+                    <li>
+                        联系我们
+                    </li>
+                    <li>
+                        中文
+                    </li>
+                    <li>
+                        English
+                    </li>
+                </ul>
+                <ul className={styles.contact}>
+                    <li>
+                        <svg className={icon.icon} style={{fontSize: '22px'}} aria-hidden="true">
+                            <use xlinkHref="#icon-gongzhonghao"></use>
+                        </svg>
+                    </li>
+                    <li onClick={() => handleOpen('https://github.com/zhounie')}>
+                        <svg className={icon.icon} style={{fontSize: '22px'}} aria-hidden="true">
+                            <use xlinkHref="#icon-github"></use>
+                        </svg>
+                    </li>
+                </ul>
+            </div>
         </footer>
     )
 }
--- pages/detail.js
+++ pages/detail.js
@@ -1,9 +1,37 @@
 import Header from '../components/Header'
+import Footer from '../components/Footer'
+import styles from '../styles/detail.less'
+
+// import Editor from 'editor.md'
+
+
+import { useEffect } from 'react'
+
+import marked from 'marked'
 export default function Detail() {
+    
+    useEffect(() => {
+        // const $ = require('jquery')(window)
+        // require('../static/js/editormd.min.js')
+        // editormd.markdownToHTML("content", {
+        //     htmlDecode: "style,script,iframe",
+        //     emoji: true,
+        //     taskList: true,
+        //     tocm: true,
+        //     tex: true, // 默认不解析
+        //     flowChart: true, // 默认不解析
+        //     sequenceDiagram: true, // 默认不解析
+        //     codeFold: true
+        // })
+        document.getElementById('content').innerHTML = marked('# Marked in the browser\n\nRendered by **marked**.')
+    }, [])
     return (
-        <div>
+        <div className={styles.detail}>
             <Header></Header>
-            detail
+            <main className={styles.main}>
+                <div id="content"></div>
+            </main>
+            <Footer></Footer>
         </div>
     )
 }
